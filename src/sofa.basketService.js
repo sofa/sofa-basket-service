f974acf1eebdf85f83fabc7371427db2255e7747
Nick Janssen <info@nickjanssen.com>
2014-03-13 17:05:29 +0100

    fix(basketService): do not include shipping for surcharge calculation

--- src/sofa.basketService.js
+++ src/sofa.basketService.js
@@ -481,7 +481,7 @@ sofa.define('sofa.BasketService', function (storageService, configService, optio
             shipping = options.shippingMethod.price;
         }
 
-        total = sum + shipping + discount;
+        total = sum + discount;
 
         /* jshint camelcase: false */
         if (surcharge_percentage) {
@@ -489,7 +489,7 @@ sofa.define('sofa.BasketService', function (storageService, configService, optio
         }
         /* jshint camelcase: true */
 
-        total += surcharge;
+        total += shipping + surcharge;
 
         // For each coupon, subtract the discount value
         activeCoupons.forEach(function (coupon) {
